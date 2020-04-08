b264a2b53ed68dabd0eb0f140f87237e1087a61d
Ben Junya <bjunya@gmail.com>
2020-04-07 18:58:13 -0700

    Initial Commit. Play with options

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,13 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useStaticQuery, graphql } from 'gatsby';
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header';
+import './layout.css';
+import 'sanitize.css';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -21,7 +22,7 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
   return (
     <>
@@ -31,9 +32,9 @@ const Layout = ({ children }) => {
           margin: `0 auto`,
           maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
+          fontSize: '62.5%'
+        }}>
+        <main style={{ fontSize: '1rem' }}>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
@@ -41,11 +42,11 @@ const Layout = ({ children }) => {
         </footer>
       </div>
     </>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired
+};
 
-export default Layout
+export default Layout;
