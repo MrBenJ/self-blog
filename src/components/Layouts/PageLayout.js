0538439990bcffb9a3a59cf31f7effc3194adf89
Ben Junya <bjunya@gmail.com>
2020-04-18 14:02:58 -0700

    Add emotin. Refactor PageLayout. Add own footer

--- src/components/Layouts/PageLayout.js
+++ src/components/Layouts/PageLayout.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import { css } from '@emotion/core';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
 
-import Header from '../header';
+import Header from '../Header';
 import './base.css';
 import 'sanitize.css';
 
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
 const PageLayout = props => {
   const { children } = props;
+
+  const style = css`
+    margin: 0 auto;
+    padding: 0 1rem 1.4rem;
+    max-width: 960px;
+
+    main {
+      font-size: 1rem;
+    }
+  `;
+
+  /*
+   * See: https://www.gatsbyjs.org/docs/use-static-query/
+   */
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -27,19 +36,9 @@ const PageLayout = props => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-          fontSize: '62.5%'
-        }}>
-        <main style={{ fontSize: '1rem' }}>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+      <div css={style}>
+        <main>{children}</main>
+        <footer>&copy; {new Date().getFullYear()} Ben Junya</footer>
       </div>
     </>
   );
